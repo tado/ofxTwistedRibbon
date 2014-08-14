292b6f20ad4bce250cfdd424f64e989dffd58492
Atsushi Tadokoro <tadokoro@gmail.com>
2014-08-14 19:06:39 +0900

    fix indent

--- src/ofxTwistedRibbon.cpp
+++ src/ofxTwistedRibbon.cpp
@@ -27,24 +27,24 @@ void ofxTwistedRibbon::draw(){
     ofSetColor(color);
     
     ofVboMesh mesh;
-	mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
-
-	for(unsigned int i = 1; i < points.size(); i++){
-		ofVec3f thisPoint = points[i-1];
-		ofVec3f nextPoint = points[i];
-		ofVec3f direction = (nextPoint - thisPoint);
-		float distance = direction.length();
-		ofVec3f unitDirection = direction.normalized();
+    mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+    
+    for(unsigned int i = 1; i < points.size(); i++){
+        ofVec3f thisPoint = points[i-1];
+        ofVec3f nextPoint = points[i];
+        ofVec3f direction = (nextPoint - thisPoint);
+        float distance = direction.length();
+        ofVec3f unitDirection = direction.normalized();
         
         ofVec3f toTheLeft = unitDirection.getRotated(90, ofVec3f(0, 1, 1));
-		ofVec3f toTheRight = unitDirection.getRotated(0, ofVec3f(0, 1, 1));
+        ofVec3f toTheRight = unitDirection.getRotated(0, ofVec3f(0, 1, 1));
         
-		ofVec3f leftPoint = thisPoint+toTheLeft * thickness;
-		ofVec3f rightPoint = thisPoint+toTheRight * thickness;
+        ofVec3f leftPoint = thisPoint+toTheLeft * thickness;
+        ofVec3f rightPoint = thisPoint+toTheRight * thickness;
         
-		mesh.addVertex(ofVec3f(leftPoint.x, leftPoint.y, leftPoint.z));
-		mesh.addVertex(ofVec3f(rightPoint.x, rightPoint.y, rightPoint.z));
-	}
+        mesh.addVertex(ofVec3f(leftPoint.x, leftPoint.y, leftPoint.z));
+        mesh.addVertex(ofVec3f(rightPoint.x, rightPoint.y, rightPoint.z));
+    }
     
-	mesh.draw();
+    mesh.draw();
 }
